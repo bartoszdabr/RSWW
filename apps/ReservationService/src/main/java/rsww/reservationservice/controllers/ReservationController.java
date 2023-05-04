5a08d36a3bed07c5271a27e557dd6c9b6ad32745
Bartosz Dabrowski <bartosz.dabrowski@intel.com>
2023-05-04 20:10:38 +0200

    Initial reservation service structure

---
+++ apps/ReservationService/src/main/java/rsww/reservationservice/controllers/ReservationController.java
@@ -0,0 +1,8 @@
+package rsww.reservationservice.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+
+}
---
+++ apps/ReservationService/src/test/java/rsww/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rsww.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
