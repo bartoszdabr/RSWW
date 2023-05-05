0bf97e6d04f1e16afec49730425c25c9601dc2c2
ulxori <grzegorzrychlicki2000@gmail.com>
2023-05-05 16:56:54 +0200

    Add initial transport service structure.

---
+++ apps/transport/transport-read-service/src/main/java/transport/read/TransportReadService.java
@@ -0,0 +1,11 @@
+package transport.read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportReadService {
+  public static void main(String[] args) {
+    SpringApplication.run(TransportReadService.class, args);
+  }
+}
