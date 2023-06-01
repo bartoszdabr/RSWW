1ec8349bfd57b911b36efc77b225f79726b6a38f
Bartosz Dabrowski <bartosz.dabrowski@intel.com>
2023-06-01 20:16:18 +0200

    Update transport return model

--- apps/transport/transport-read-service/src/main/java/transport/models/TransportResponseModel.java
+++ apps/transport/transport-read-service/src/main/java/transport/models/TransportResponseModel.java
@@ -1,11 +1,13 @@
 package transport.models;
 
 import lombok.Builder;
+import lombok.Getter;
 import transport.read.entity.Transport;
 
 import java.util.List;
 
 @Builder
+@Getter
 public class TransportResponseModel {
 
     private final List<Transport> matchingTransports;
