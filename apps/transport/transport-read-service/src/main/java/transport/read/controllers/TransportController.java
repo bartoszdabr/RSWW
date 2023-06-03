1903edd75c3784f37b242001fa10233db1ed2981
ulxori <grzegorzrychlicki2000@gmail.com>
2023-06-03 15:23:05 +0200

    Fix

--- apps/transport/transport-read-service/src/main/java/transport/read/controllers/TransportController.java
+++ apps/transport/transport-read-service/src/main/java/transport/read/controllers/TransportController.java
@@ -1,7 +1,10 @@
 package transport.read.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import transport.models.TransportResponseModel;
 import transport.read.entity.Transport;
 import transport.read.services.TransportService;
@@ -10,7 +13,6 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin
 public class TransportController {
   private final TransportService transportService;
 
