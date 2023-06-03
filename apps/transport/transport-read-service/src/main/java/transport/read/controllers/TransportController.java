0d69c81e04f19be68fb2f70acac1d233affedd01
ulxori <grzegorzrychlicki2000@gmail.com>
2023-06-03 13:25:35 +0200

    Update url prefix, add endpoint for transport with specified id.

--- apps/transport/transport-read-service/src/main/java/transport/read/controllers/TransportController.java
+++ apps/transport/transport-read-service/src/main/java/transport/read/controllers/TransportController.java
@@ -2,6 +2,7 @@ package transport.read.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import transport.models.TransportResponseModel;
@@ -9,7 +10,6 @@ import transport.read.entity.Transport;
 import transport.read.services.TransportService;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -33,5 +33,12 @@ public class TransportController {
       @RequestParam("date") Optional<LocalDate> date) {
 
     return ResponseEntity.ok(transportService.findTransports(sourcePlace, destinationPlace, date, numOfPeople));
+  }
+
+  @GetMapping("transports/{id}")
+  public ResponseEntity<Transport> getTransport(@PathVariable String id) {
+    Optional<Transport> transport = transportService.findTransport(id);
+    return transport.map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
   }
 }
