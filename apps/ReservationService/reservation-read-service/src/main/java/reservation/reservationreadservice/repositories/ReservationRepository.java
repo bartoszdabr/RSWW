316649e33f8c4741cf8d8b73d688cf3b974925d8
Bartosz Dabrowski <bartosz.dabrowski@intel.com>
2023-06-04 18:08:36 +0200

    Update query logic

--- apps/ReservationService/reservation-read-service/src/main/java/reservation/reservationreadservice/repositories/ReservationRepository.java
+++ apps/ReservationService/reservation-read-service/src/main/java/reservation/reservationreadservice/repositories/ReservationRepository.java
@@ -1,16 +1,15 @@
 package reservation.reservationreadservice.repositories;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import reservation.reservationreadservice.entities.ReservationEntity;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends MongoRepository<ReservationEntity, String> {
 
-    @Query(value = "{$and: [{roomReservationId:  '?0'}, {transportId: '?1'}, {status:  '?2'}]}")
-    List<ReservationEntity> findByHotelAndTransportIdsAndStatus(String hotelId, String transportId, String status, Sort sort);
+    Optional<ReservationEntity> findFirstByRoomReservationIdAndTransportIdAndStatusOrderByTimestampDesc(String roomReservationId,
+                                                                                                        String transportId,
+                                                                                                        String status);
 }
--- apps/ReservationService/reservation-read-service/src/main/java/reservation/reservationreadservice/services/NotificationService.java
+++ apps/ReservationService/reservation-read-service/src/main/java/reservation/reservationreadservice/services/NotificationService.java
@@ -4,8 +4,6 @@ package reservation.reservationreadservice.services;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import reservation.events.ReservationStatuses;
@@ -26,13 +24,11 @@ public class NotificationService {
 
     public Instant checkIfBought(String hotelId, String transportId) {
         log.info("Checking db for reservation for hotelId: " + hotelId + " and transportId: " + transportId);
-        var reservations =  reservationRepository.findByHotelAndTransportIdsAndStatus(hotelId, transportId,
-                ReservationStatuses.RESERVED.getText(), Sort.by(Sort.Direction.DESC, "timestamp"));
+        var reservation =  reservationRepository.findFirstByRoomReservationIdAndTransportIdAndStatusOrderByTimestampDesc(hotelId, transportId,
+                ReservationStatuses.RESERVED.getText());
 
-        if (reservations.isEmpty()) {
-            throw new ResponseStatusException(NOT_FOUND, "No buy events for provided ids");
-        }
-
-        return reservations.get(0).getTimestamp();
+        return reservation
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "No buy events for provided ids"))
+                .getTimestamp();
     }
 }
